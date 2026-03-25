80ff92c6a9b26dc4fc4f41ccd178159dc22f3748
woeihaw.ang <woeihaw.ang@hytechc.com>
2026-03-26 07:31:01 +0800

    add query param

--- books.py
+++ books.py
@@ -25,3 +25,19 @@ async def read_books(book_title: str):
             return book
     return None
 
+@app.get("/books/")
+async def read_category_ny_query(category:str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author:str,category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("author").casefold() == book_author.casefold() and \
+            book.get("category").casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
